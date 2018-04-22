da2095f469a97310a7ddf5b241c2cfae685dec40
Carlos Adriano <adrianobeserra@gmail.com>
2018-04-21 23:30:56 -0300

    Visualização de Documentos

--- routes/index.js
+++ routes/index.js
@@ -6,38 +6,50 @@ const usuarioController = require('../controllers/usuarioController');
 var jsonParser = bodyParser.json();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  console.log('teste');
+router.get('/', function (req, res) {
   global.db.findAll((e, docs) => {
-      if(e) { return console.log(e); }
-      res.render('index', { title: 'Lista de Documentos', docs: docs });
+    if (e) { return console.log(e); }
+    res.render('index', { title: 'Lista de Documentos', docs: docs });
   })
 })
 
+/* GET Usuario por ID. */
+router.get('/:id', function (req, res) {
+  let id = req.param.id;
+  if (id) {
+    global.db.findOne(id, (e, docs) => {
+      if (e) {
+        return console.log(e);
+      }
+      res.status(201).send(JSON.stringify(docs));
+    })
+  }
+});
+
 
 /* Rota para a View de Cadastro */
-router.get('/new',function(req, res, next) {
+router.get('/new', function (req, res, next) {
   res.render('new', { title: 'Novo Documento' });
 });
 
 /* Rota para a Cadastro de Usuário */
-router.post('/new', function(req, res, next) {
+router.post('/new', function (req, res, next) {
   usuarioController.post(req, res, next);
   //res.render('new', { title: 'Novo Cadastro' });
 });
 
 /* Rota para a View de Update */
-router.get('/update/:id', function(req, res, next) {
+router.get('/update/:id', function (req, res, next) {
   usuarioController.findOne(req, res, next);
 });
 
 /* Rota para Update */
-router.post('/update/:id', function(req, res, next) {
+router.post('/update/:id', function (req, res, next) {
   usuarioController.put(req, res, next);
 });
 
 /* Rota para Delete */
-router.get('/delete/:id', function(req, res, next) {
+router.get('/delete/:id', function (req, res, next) {
   usuarioController.delete(req, res, next);
 });
 
